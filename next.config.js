23a0ed2e3f7ab078c81ff7e677f53779e63d4f9b
Wagner <wagnerdmll1994@gmail.com>
2022-09-18 11:02:24 -0300

    Main video view

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["th.bing.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

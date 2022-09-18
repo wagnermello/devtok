8cd3819c4d3528cdb5a3fb351329d2d9c342a3be
Wagner <wagnerdmll1994@gmail.com>
2022-09-18 07:25:51 -0300

    Sidebar

---
+++ components/Discover.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+
+import { topics } from "../utils/constants";
+
+const Discover = () => {
+	return <div>Discover</div>;
+};
+
+export default Discover;

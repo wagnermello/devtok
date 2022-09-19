35c7674f9590bc301011d4ba3cab6b7939f4810f
Wagner <wagnerdmll1994@gmail.com>
2022-09-19 13:23:44 -0300

    Comments

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -25,8 +25,9 @@ const Detail = ({ postDetails }: IProps) => {
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const router = useRouter();
 	const { userProfile }: any = useAuthStore();
-
 	const videoRef = useRef<HTMLVideoElement>(null);
+	const [comment, setComment] = useState("");
+	const [isPostingComment, setIsPostingComment] = useState(false);
 
 	const onVideoClick = () => {
 		if (playing) {
@@ -54,6 +55,22 @@ const Detail = ({ postDetails }: IProps) => {
 				like,
 			});
 			setPost({ ...post, likes: res.data.likes });
+		}
+	};
+
+	const addComment = async (e) => {
+		e.preventDefault();
+
+		if (userProfile && comment) {
+			setIsPostingComment(true);
+
+			const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+				userId: userProfile._id,
+				comment,
+			});
+			setPost({ ...post, comments: data.comments });
+			setComment("");
+			setIsPostingComment(false);
 		}
 	};
 
@@ -141,7 +158,13 @@ const Detail = ({ postDetails }: IProps) => {
 							/>
 						)}
 					</div>
-					<Comments />
+					<Comments
+						comment={comment}
+						setComment={setComment}
+						addComment={addComment}
+						comments={post.comments}
+						isPostingComment={isPostingComment}
+					/>
 				</div>
 			</div>
 		</div>
